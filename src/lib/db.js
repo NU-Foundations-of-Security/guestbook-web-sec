77f8a02ee50bda6a3edee102d4ab7ddacc7cb266
Sruti Bhagavatula <srutib@cmu.edu>
2024-04-23 08:42:48 -0500

    Database new user

--- src/lib/db.js
+++ src/lib/db.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2');
 export const getDatabase = () => {
   const db = mysql.createConnection({
     multipleStatements: true,
-    host     : '0.tcp.ngrok.io',
-    port     : 12254,
-    user     : 'root',
-    password : '', //Not secure to store it this way; fill in yourself
+    host     : '6.tcp.ngrok.io',
+    port     : 11283,
+    user     : '396user',
+    password : '396user', //Not secure to store it this way; fill in yourself
     database : 'guestbook'
   });
 
